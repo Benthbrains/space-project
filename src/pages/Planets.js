956a0b3c8d73a5ae32dce47f59b6207809f416fd
Benthbrains <benjamin.iranifey@gmail.com>
2025-10-02 20:08:23 +0200

    Delete src directory

--- src/pages/Planets.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Planets() {
-  return (
-    <section>
-      <h1 style={{color:'#0ef'}}>🪐 Planets & Exoplanets</h1>
-      <p style={{fontSize:'1.2rem',maxWidth:700,margin:'0 auto 24px',lineHeight:1.7,background:'rgba(10,10,35,0.7)',padding:'18px 24px',borderRadius:16,boxShadow:'0 0 12px #0ef'}}>Discover our solar system and the most important exoplanets. (Add planet cards and info here!)</p>
-    </section>
-  );
-}
